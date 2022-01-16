c3f1dafa562b17bf03c9c5ca453ac1cd024876e0
unknown <patrickbyann@gmail.com>
2022-01-17 00:34:07 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
 import './App.css';
+import * as htmlToImage from 'html-to-image';
+import template from './template.png'
+import db from './db'
+import Swal from 'sweetalert2';
+import download from 'downloadjs';
 
 function App() {
+  const [ count, setCount ] = useState(0)
+  const [ printed, setPrinted ] = useState(false)
+  const [ text, setText ] = useState('')
+
+  useEffect(() => {
+    // next()
+   
+    if(!printed){
+      print()
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [count])
+
+  const print = async () => {
+    const result = await Swal.fire({
+      title: 'Name',
+      input: 'text',
+      inputAttributes: {
+        autocapitalize: 'off'
+      },
+      showCancelButton: true,
+      confirmButtonText: 'Enter',
+      showLoaderOnConfirm: true,
+      preConfirm: (e) => {
+        setText(e)
+        return e
+      }
+    })
+    if(result.isConfirmed){
+      const { value } = result
+  
+      // setText(value)
+      await generate({ type: 'text', value })
+      setPrinted(true)
+    }
+  }
+
+  const next = async () => {
+    let lastCount = count
+
+    const confirm = await Swal.fire(
+      'next',
+      `count ${lastCount}, name: ${db[lastCount]}`,
+      'success'
+    )
+    if(confirm.isConfirmed){
+      await generate({ type: 'index', value: lastCount})
+    }
+
+  }
+
+  const generate = async ({ type, value }) => {
+    const pic = document.getElementById('pic');
+  
+    htmlToImage.toJpeg(pic, { quality: 1 })
+    .then((dataUrl) => {
+      if(type === 'text') {
+        download(dataUrl, `${value}.jpeg`);
+      } else {
+        download(dataUrl, `${db[value]}.jpeg`);
+      }
+      // i++
+      // setCount(i)
+    })
+    .catch((err) => {
+      Swal.fire('error', err.message, 'error')
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="pic" style={{ position: 'relative' }}>
+      <img src={template} alt="template" style={{width: '100%'}} />
+      <div className='name'>
+        { !!text.length? text : db[count] }
+      </div>
+      <div className='n'>
+        1
+      </div>
     </div>
   );
 }
---
+++ src/db.js
@@ -0,0 +1,61 @@
+const db = [ 
+    'Pak Yohanes',
+    'Ko Khiong',
+    'Cik Kong',
+    'Ce Ni',
+    'Bik Nyo',
+    'Mr. Bu',
+    'Mi Mi',
+    'Ho Ho',
+    'Yansen',
+    'Sintia',
+    
+    'Yana',
+    'Sakpo',
+    'Thu Yun',
+    'Cerren',
+    'Erick',
+    'Belinda',
+    'Sherlyn',
+    'Jodi',
+    'Lia',
+    'Billy',
+    
+    
+    'Ko Kong',
+    'Pakme',
+    'Ku Tin',
+    'Ku Chong A Ngian',
+    'Ku Chong Denny',
+    'Ii Alan',
+    'Hel Hel',
+    'Epen',
+    'Ce Chong Akiat', //
+    'Anton', //
+    
+    
+    'Romo Rudi',
+    'Ko Willy',
+    'Romo Tolas',
+    'Hartojo',
+    'Lie Kian',
+    'Naga',
+    'Joni',
+    'Benkam',
+    'Mm',
+    'Bunda',
+    
+    
+    'Yenti',
+    'Ervi',
+    'Fenty',
+    'Rina',
+    'Sisil',
+    'Mei Mei',
+    'Lucy',
+    'Aphin',
+    'Teti',
+    'Indah',
+]
+
+export default db
